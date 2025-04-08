3906e0d623db8c5247e61bd95d8c86db092f91f5
“tkd992006” <tkd992006@naver.com>
2025-04-08 22:48:25 +0900

    asdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 
-const router = createBrowserRouter([{ path: "/gagageul", element: <App /> }]);
+// const router = createBrowserRouter([{ path: "/gagageul", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 		<div className="noto-sans-kr h-full min-h-0">
-			<RouterProvider router={router} />
+			<App />
 		</div>
 	</ThemeProvider>
 );
